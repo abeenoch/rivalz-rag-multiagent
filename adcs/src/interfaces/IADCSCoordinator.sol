f7e0db8e2ac8745a017b265cb53e025d32bca030
Abe Enoch Aboluwarin <60470721+abeenoch@users.noreply.github.com>
2024-12-16 12:34:53 +0100

    initial commit

--- adcs/src/interfaces/IADCSCoordinator.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
-
-import "./IADCSCoordinatorBase.sol";
-import "./ICoordinatorBase.sol";
-
-interface IADCSCoordinator is IADCSCoordinatorBase, ICoordinatorBase {}
